9bf24b0c425758339405804c83cc51199a201883
admin <admin@COMP1>
2017-04-16 15:43:10 +0500

    Добавлен метод поиска пути в невзвешенном графе FindPath

--- GraphsLib/ExtensionsGraph.cs
+++ GraphsLib/ExtensionsGraph.cs
@@ -12,7 +12,7 @@ namespace GraphsLib
         {
             var result = new List<List<Node>>();
             var markedNodes = new HashSet<Node>();
-            while(true)
+            while (true)
             {
                 var nextNode = graph.Nodes.Where(node => !markedNodes.Contains(node)).FirstOrDefault();
                 if (nextNode == null) break;
@@ -22,6 +22,41 @@ namespace GraphsLib
                     markedNodes.Add(node);
             }
             return result;
+        }
+
+
+        public static List<Node> FindPath(this Graph graph, int start, int end)
+        {
+            if (start >= graph.Length || end >= graph.Length) throw new ArgumentException();
+            var startNode = graph[start];
+            var endNode = graph[end];
+            var track = new Dictionary<Node, Node>();
+            track[startNode] = null;
+            var queue = new Queue<Node>();
+            queue.Enqueue(startNode);
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                foreach (var nextNode in node.IncidentNodes)
+                {
+                    if (track.ContainsKey(nextNode)) continue;
+                    track[nextNode] = node;
+                    queue.Enqueue(nextNode);
+                }
+                if (track.ContainsKey(endNode)) break;
+            }
+            if (!track.ContainsKey(endNode)) return null;
+            var result = new List<Node>();
+            
+            var pathItem = endNode;
+            while (pathItem != null)
+            {
+                result.Add(pathItem);
+                pathItem = track[pathItem];
+            }
+            result.Reverse();
+            return result;
+
         }
     }
 }
